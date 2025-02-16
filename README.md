10810ca36652e3d46870ba98be633c1111588ba6
Darron K <darron.knowles@icloud.com>
2025-02-15 21:37:43 -0500

    v1:mail_me python program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mail_me
 Python SMTP (gmail)
+
+mail_me is a small python program that uses smtp to deliver mail based on user input. The goal is to further devlope this code and make enhancements along the way.    
---
+++ mailme.py
@@ -0,0 +1,21 @@
+""" Python SMTP program (gmail) """
+
+import smtplib
+
+__version__ = "1"
+__author__ = "@rasta0981 - Darren K."
+
+
+# -----------------------------------------------
+# Simple Python email program for sending mail
+# -----------------------------------------------
+user_email = input("Your Email: ")
+subject = input("Subject: ")
+sender_email = "<Default gmail address of sender>" # replace with sender email  
+mail_message = input("Message: ")
+text = f"Subject: {subject}\n\n{mail_message}"
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, "<gmail app code>") # requires gmail app code
+server.sendmail(sender_email, user_email, text)
+# -----------------------------------------------
